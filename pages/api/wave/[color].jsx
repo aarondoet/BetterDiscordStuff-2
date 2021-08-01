b239520e33b8bf9d30297dd0978f87f27a2f9e3d
Taimoor-Tariq <taimoorhero15@gmail.com>
2021-07-31 22:03:50 -0400

    Update Wave

--- pages/api/wave/[color].jsx
+++ pages/api/wave/[color].jsx
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
     const { color } = req.query;
     res.setHeader('Content-Type', 'image/svg+xml');
-    res.end(`<svg width="268" height="20" viewBox="0 0 268 20" xmlns="http://www.w3.org/2000/svg"><path fill="#${color}" d="M0 7.05426C8.02604 4.70284 32.16 0 64.4875 0C96.815 0 123.322 4.92178 134 7.05426C145.516 9.35401 175.247 13.9535 202.047 13.9535C228.847 13.9535 257.182 9.35401 268 7.05426V20H0V7.05426Z"/></svg>`);
+    res.end(`<svg width="300" height="20" viewBox="0 0 300 20" xmlns="http://www.w3.org/2000/svg"><path fill="#${color}" d="M300 7.05426C291.016 4.70284 264 0 227.812 0C191.625 0 161.953 4.92178 150 7.05426C137.109 9.35401 103.828 13.9535 73.8281 13.9535C43.8281 13.9535 12.1094 9.35401 0 7.05426V20H300V7.05426Z"/></svg>`);
 }
